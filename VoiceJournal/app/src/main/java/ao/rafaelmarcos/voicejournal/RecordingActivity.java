f4e3bb8b84449433e183c695a49a8d56384a869a
Rafael Marcos <rafix995@gmail.com>
2024-02-01 09:08:10 +0100

    audio recoding permission

--- VoiceJournal/app/src/main/java/ao/rafaelmarcos/voicejournal/RecordingActivity.java
+++ VoiceJournal/app/src/main/java/ao/rafaelmarcos/voicejournal/RecordingActivity.java
@@ -1,14 +1,35 @@
 package ao.rafaelmarcos.voicejournal;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class RecordingActivity extends AppCompatActivity {
     private TextView mTimerTextView;
     private FloatingActionButton mStopFab;
+
+    private final int AUDIO_PERMISSION_REQUEST_CODE = 200; //WHY 200?
+    private boolean mHasRecodingPermission;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permission, grantResults);
+
+        if(requestCode == AUDIO_PERMISSION_REQUEST_CODE){
+            mHasRecodingPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            if(!mHasRecodingPermission)
+                finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +43,14 @@ public class RecordingActivity extends AppCompatActivity {
             String timerText = String.format(getString(R.string.timer), 99);
             mTimerTextView.setText(timerText);
         });
+
+        // Request recording permission
+        String[] recordingPermission = {Manifest.permission.RECORD_AUDIO};
+        ActivityCompat.requestPermissions(this, recordingPermission, AUDIO_PERMISSION_REQUEST_CODE);
+    }
+
+    private void startRecording(){
+        // TODO record audio
+        Toast.makeText(this, "Recording", Toast.LENGTH_SHORT).show();
     }
 }
