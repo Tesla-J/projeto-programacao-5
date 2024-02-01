a1e7a2cd44c7dc69741ee690a762f67f16b50078
Rafael Marcos <rafix995@gmail.com>
2024-02-01 09:49:25 +0100

    Player: will play, pause and stop the recorded audio

--- VoiceJournal/app/src/main/java/ao/rafaelmarcos/voicejournal/player/Player.java
+++ VoiceJournal/app/src/main/java/ao/rafaelmarcos/voicejournal/player/Player.java
@@ -1,2 +1,41 @@
-package ao.rafaelmarcos.voicejournal.player;public class Player {
+package ao.rafaelmarcos.voicejournal.player;
+
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class Player {
+    private boolean isPlaying;
+    private MediaPlayer player;
+
+    public void play(String filePath){
+        if(player != null)
+            player.start();
+        else{
+            player = new MediaPlayer();
+            try{
+                player.setDataSource(filePath);
+                player.prepare();
+                player.start();
+            }
+            catch(IOException ioe){
+                Log.e("PLAY AUDIO FAILED", "Failed to play audio");
+                ioe.printStackTrace();
+            }
+        }
+    }
+
+    public void pause(){
+        if(player != null)
+            player.pause();
+    }
+
+    public void stop(){
+        if(player != null){
+            player.stop();
+            player.release();
+            player = null;
+        }
+    }
 }
